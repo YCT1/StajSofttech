66d98c9ded6046cfbcdbfd60049c8269938c475f
Yekta Can Tursun <yektacan1999@gmail.com>
2021-09-03 17:20:09 +0300

    Projection View has been created

--- MUNVoter/Controllers/ViewController.cs
+++ MUNVoter/Controllers/ViewController.cs
@@ -26,5 +26,23 @@ namespace MUNVoter.Controllers
             ViewBag.countryImg = db.CountryFlags.FindImageAddressesByMotions(motions);
             return View(motions);
         }
+
+        [Route("View/Projection/{sessionID?}")]
+        public ActionResult ProjectionView(int sessionID)
+        {
+            var db = new UnitOfWork(new DatabaseContext());
+            if (!db.Sessions.isSessionExsist(sessionID))
+            {
+                return Content("<h2>The session does not exsist</h2>");
+            }
+
+            ViewBag.motionNumber = db.Motions.GetMotionNumberBySessionId(sessionID);
+            ViewBag.SessionID = sessionID;
+            ViewBag.ConferenceName = db.Sessions.findSessionById(sessionID).ConferenceName;
+            ViewBag.ComitteeName = db.Sessions.findSessionById(sessionID).CommitteeName;
+            List<Motion> motions = db.Motions.GetMotionsBySessionId(sessionID).ToList();
+            ViewBag.countryImg = db.CountryFlags.FindImageAddressesByMotions(motions);
+            return View(motions);
+        }
     }
 }
